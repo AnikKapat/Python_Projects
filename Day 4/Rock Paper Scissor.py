f868d2c9b1b4006c9b8800663c17bdb0c060248d
Anik Kapat <kapatak47@gmail.com>
2025-07-31 17:23:17 +0530

    Added Day 4 - Rock Paper Scissors project

---
+++ Day 4/Rock Paper Scissor.py
@@ -0,0 +1,63 @@
+# Day 4 Project - Rock Paper Scissors
+
+import random
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+# Store the images in a list
+game_images = [rock, paper, scissors]
+
+print("Welcome to Rock Paper Scissors!")
+
+# Get user choice
+user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors: "))
+
+# Check if user input is valid
+if user_choice >= 3 or user_choice < 0:
+    print("You typed an invalid number, you lose!")
+else:
+    # Show user's choice
+    print("You chose:")
+    print(game_images[user_choice])
+
+    # Generate computer choice
+    computer_choice = random.randint(0, 2)
+
+    # Show computer's choice
+    print("Computer chose:")
+    print(game_images[computer_choice])
+
+    # Determine winner
+    if user_choice == computer_choice:
+        print("It's a draw! 🤝")
+    elif (user_choice == 0 and computer_choice == 2) or \
+            (user_choice == 1 and computer_choice == 0) or \
+            (user_choice == 2 and computer_choice == 1):
+        print("You win! 🎉")
+    else:
+        print("You lose! 😞")
